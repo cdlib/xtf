0df8e523f77377069ac3a9f3b13171f56b8ff1b3
jpgovaars <none@none>
2004-11-19 21:18:16 +0000

    Added more utility functions to make formatting attributes easier.
    --HG--
    extra : convert_revision : svn%3A5c944932-937e-0410-aa5c-9344c804e6c3/trunk%4043

--- WEB-INF/src/org/cdlib/xtf/util/FormatXML.java
+++ WEB-INF/src/org/cdlib/xtf/util/FormatXML.java
@@ -35,44 +35,44 @@ import java.util.LinkedList;
 ////////////////////////////////////////////////////////////////////////////////
 
 /** This class provides a simple way to produce indented XML with matched
-    begin and end tags. <br><br>
-
-    To use this class, you basically do the following: <br><br>
-
-    <code>
-    Writer XMLDoc = new OutputStreamWriter(
-                          new FileOutputStream( XMLFilePath )
-                        );
-
-    FormatXML.tabSize(4); // (Defaults to 2 spaces if not specified)
-
-    XMLDoc.write(
-      FormatXML.procInstr( "xml version=\"1.0\" encoding=\"utf-8\"" )
-    );
-
-    XMLDoc.write(
-      FormatXML.beginTag( "tagName", "tagAttr=\"value\"" )
-    );
-
-    XMLDoc.write(
-      FormatXML.text( "A bunch of text within a tag." ) + newLine(2)
-    );
-
-    XMLDoc.write(
-      FormatXML.endTag()
-    );
-    </code> <br><br>
-
-    This will produce an XML file with the following contents: <br><br>
-
-    &lt;?xml version="1.0" encoding="utf-8"?&gt;
-
-    &lt;tagName tagAttr="value"&gt;
-
-        A bunch of text within a tag.
-
-    &lt;/tagName&gt;
-*/
+ *   begin and end tags. <br><br>
+ *
+ *    To use this class, you basically do the following: <br><br>
+ *
+ *    <code>
+ *    Writer XMLDoc = new OutputStreamWriter(
+ *                          new FileOutputStream( XMLFilePath )
+ *                        );
+ *
+ *    FormatXML.tabSize(4); // (Defaults to 2 spaces if not specified)
+ *
+ *    XMLDoc.write(
+ *      FormatXML.procInstr( "xml version=\"1.0\" encoding=\"utf-8\"" )
+ *    );
+ *
+ *    XMLDoc.write(
+ *     FormatXML.beginTag( "tagName", "tagAttr=\"value\"" )
+ *    );
+ *
+ *    XMLDoc.write(
+ *      FormatXML.text( "A bunch of text within a tag." ) + newLine(2)
+ *    );
+ *
+ *    XMLDoc.write(
+ *      FormatXML.endTag()
+ *    );
+ *    </code> <br><br>
+ *
+ *    This will produce an XML file with the following contents: <br><br>
+ *
+ *    &lt;?xml version="1.0" encoding="utf-8"?&gt;
+ *
+ *    &lt;tagName tagAttr="value"&gt;
+ *
+ *        A bunch of text within a tag.
+ *
+ *    &lt;/tagName&gt;
+ */
 public class FormatXML {
 
   /** Default amount to indent when {@link #tab()} is called. */
@@ -81,17 +81,17 @@ public class FormatXML {
   //////////////////////////////////////////////////////////////////////////////
 
   /** Return whether or not a blank line will automatically be inserted after
-      each new tag. <br><br>
-
-      The default behavior is to insert a blank line after each tag. To change
-      this behavior, call the
-      {@link FormatXML#blankLineAfterTag(boolean) blankLineAfterTag(boolean enable) }
-      function. <br><br>
-
-      @return
-         <code>true</code> - Blank lines will be inserted after each new tag.
-         <code>false</code> - No blank lines will be inserted after each new tag.
-  */
+   *  each new tag. <br><br>
+   *
+   *  The default behavior is to insert a blank line after each tag. To change
+   *  this behavior, call the
+   *  {@link FormatXML#blankLineAfterTag(boolean) blankLineAfterTag(boolean enable) }
+   *  function. <br><br>
+   *
+   *  @return
+   *     <code>true</code> - Blank lines will be inserted after each new tag.
+   *     <code>false</code> - No blank lines will be inserted after each new tag.
+   */
   public static final boolean blankLineAfterTag()
 
   {
@@ -104,15 +104,15 @@ public class FormatXML {
   //////////////////////////////////////////////////////////////////////////////
 
   /** Set whether or not a blank line will automatically be inserted after
-      each new tag. <br><br>
-
-      @param enable   Enable (<code>true</code>) or disable (<code>false</code>)
-                      automatic blank line insertion after each tag.
-
-
-      @return
-          The value of the blank line flag just prior to this call.
-  */
+   *  each new tag. <br><br>
+   *
+   *  @param enable   Enable (<code>true</code>) or disable (<code>false</code>)
+   *                  automatic blank line insertion after each tag.
+   *
+   *
+   *  @return
+   *      The value of the blank line flag just prior to this call.
+   */
   public static final boolean blankLineAfterTag( boolean enable )
 
   {
@@ -132,15 +132,15 @@ public class FormatXML {
   //////////////////////////////////////////////////////////////////////////////
 
   /** Return the current tab size used for indenting nested tags. <br><br>
-
-      By default, the tab size is set to 2 spaces. To change the indent size,
-      call the {@link FormatXML#tabSize(int) tabSize(int newTabSize) }
-      method.
-
-      @return
-          The number of spaces each a nested line will be indented from its
-          container.
-  */
+   *
+   *  By default, the tab size is set to 2 spaces. To change the indent size,
+   *  call the {@link FormatXML#tabSize(int) tabSize(int newTabSize) }
+   *  method.
+   *
+   *  @return
+   *      The number of spaces each a nested line will be indented from its
+   *      container.
+   */
   public static final int tabSize()
 
   {
@@ -153,14 +153,14 @@ public class FormatXML {
   //////////////////////////////////////////////////////////////////////////////
 
   /** Set the tab size used for indenting nested tags. <br><br>
-
-      @param newTabSize  The new tab size (in spaces) to indent a nested tag
-                         from its containing tag.
-
-      @return
-          The number of spaces each a nested line will be indented from its
-          container.
-  */
+   *
+   *  @param newTabSize  The new tab size (in spaces) to indent a nested tag
+   *                     from its containing tag.
+   *
+   *  @return
+   *      The number of spaces each a nested line will be indented from its
+   *      container.
+   */
   public static final int tabSize( int newTabSize )
 
   {
@@ -184,10 +184,10 @@ public class FormatXML {
   //////////////////////////////////////////////////////////////////////////////
 
   /** Return a string containing a properly indented begin tag consisting
-      only of the tag name. <br><br>
-
-      @param tagName  The name of the tag to create.
-  */
+   *  only of the tag name. <br><br>
+   *
+   *  @param tagName  The name of the tag to create.
+   */
   public static final String beginTag( String tagName )
 
   {
@@ -214,51 +214,66 @@ public class FormatXML {
 
   } // public beginTag( String tagName )
 
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  /** Format a tag attribute from an attribute name and an associated string
+   *  value.
+   */
+   public static final String attr( String attName, String attValue )
+
+  {
+
+    return " " + attName + "=\"" + attValue + "\"";
+
+  } // public attr()
+
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  /** Format a tag attribute from an attribute name and an associated integer
+   *  value.
+   */
+  public static final String attr( String attName, int attValue )
+
+  {
+
+    return " " + attName + "=\"" + Integer.toString(attValue) + "\"";
+
+  } // public attr()
+
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  /** Format a tag attribute from an attribute name and an associated 
+   *  floating-point value.
+   */
+  public static final String attr( String attName, float attValue )
+
+  {
+
+    return " " + attName + "=\"" + Float.toString(attValue) + "\"";
+
+  } // public attr()
+
+
   //////////////////////////////////////////////////////////////////////////////
 
   /** Return a string containing a properly indented begin tag consisting
-      of a tag name and a list of attributes. <br><br>
-      
-      By default, this function converts single quotes that appear in the
-      tag attribute string to double quotes. To disable this conversion,
-      use the {@link FormatXML#beginTag(String,String,boolean) beginTag(String,String,boolean) }
-      function.
-
-      @param tagName   The name of the tag to create.
-      
-      @param tagAtts   A string of attributes to tadd to the tag.
-  */
+   *  of a tag name and a list of attributes. <br><br>
+   *
+   *  @param tagName        The name of the tag to create.
+   *      
+   *  @param tagAtts        A string of attributes to tadd to the tag.
+   *      
+   *  @.notes
+   *       Use the {@link FormatXML#attr() attr() } simplify constructing
+   *       attribute name/value pairs.      
+   */
   public static final String beginTag( 
   
       String  tagName, 
       String  tagAtts
-  )
-
-  {
-  
-    return beginTag( tagName, tagAtts, true );
-    
-  }
-  
-  
-  //////////////////////////////////////////////////////////////////////////////
-
-  /** Return a string containing a properly indented begin tag consisting
-      of a tag name and a list of attributes. <br><br>
-
-      @param tagName        The name of the tag to create.
-      
-      @param tagAtts        A string of attributes to tadd to the tag.
-      
-      @param convertQuotes  Flag indicating whether single quotes should
-                            be converted to double quotes (<code>true</code>)
-                            or not (<code>false</code>).
-  */
-  public static final String beginTag( 
-  
-      String  tagName, 
-      String  tagAtts,
-      boolean convertQuotes
   )
 
   {
@@ -271,13 +286,19 @@ public class FormatXML {
     //
     if( tagAtts == null || tagAtts.length() == 0 ) return beginTag( tagName );
 
-    // If the user wants to convert single quotes to double quotes, do so.
-    if( convertQuotes ) tagAtts = tagAtts.replaceAll( "'", "\"" );
-
-    // Format up the tag name and attributes between angle brackets.
-    String outStr = spaces.substring( 0, tabCount ) +
-                    "<" + tagName + " " + tagAtts +">\n";
-
+    // Format up the tag name and attributes between angle brackets. Start
+    // with the angle bracket and tag name.
+    //
+    String outStr = spaces.substring( 0, tabCount ) + "<" + tagName;
+    
+    // If the tag string doesn't start with a space, add one to separate
+    // the first attribute from the tag name.
+    //
+    if( tagAtts.charAt(0) != ' ' ) outStr += " ";
+    
+    // Then add the closing angle bracket and we're done.
+    outStr += tagAtts +">\n";
+ 
     // Add the tag name to the current nesting stack so that the
     // endTag() method can properly close the tag when called.
     //
@@ -298,8 +319,8 @@ public class FormatXML {
   //////////////////////////////////////////////////////////////////////////////
 
   /** Return a string containing a properly indented end tag for the
-      closest open tag (if any.)
-  */
+   *  closest open tag (if any.)
+   */
   public static final String endTag()
 
   {
@@ -326,8 +347,8 @@ public class FormatXML {
   //////////////////////////////////////////////////////////////////////////////
 
   /** Return a string containing properly indented end tags for any
-      remaining open tags.
-  */
+   *  remaining open tags.
+   */
   public static final String endAllTags()
 
   {
@@ -347,13 +368,13 @@ public class FormatXML {
   //////////////////////////////////////////////////////////////////////////////
 
   /** Return a processing instruction tag.
-
-      @param  tagStr  The processing instruction string to place in the tag.
-
-      @return
-          A processing instruction tag indented to the current indentation
-          level.
-  */
+   *
+   *  @param  tagStr  The processing instruction string to place in the tag.
+   *
+   *  @return
+   *      A processing instruction tag indented to the current indentation
+   *      level.
+   */
   public static final String tag( String tagStr )
 
   {
@@ -373,9 +394,9 @@ public class FormatXML {
   //////////////////////////////////////////////////////////////////////////////
 
   /** Return a processing instruction tag at the current level of indentation.
-
-      @param  procStr   The processing instruction string to place in the tag.
-  */
+   *
+   *  @param  procStr   The processing instruction string to place in the tag.
+   */
   public static final String procInstr( String procStr )
 
   {
@@ -395,9 +416,9 @@ public class FormatXML {
   //////////////////////////////////////////////////////////////////////////////
 
   /** Return a string of text at the current level of indentation.
-
-      @param  str  The text to indent.
-  */
+   *
+   *  @param  str  The text to indent.
+   */
   public static final String text( String str )
 
   {
@@ -414,15 +435,15 @@ public class FormatXML {
   //////////////////////////////////////////////////////////////////////////////
 
   /** Return a string containing text broken across multiple indented
-      lines less than or equal to a maximum line length. <br><br>
-
-      @param  str        The string to break across multiple lines.
-
-      @param  maxWidth   The maximum width for each line.
-
-      @return
-          The original input text broken across multiple lines.
-  */
+   *  lines less than or equal to a maximum line length. <br><br>
+   *
+   *   @param  str        The string to break across multiple lines.
+   *
+   *   @param  maxWidth   The maximum width for each line.
+   *
+   *   @return
+   *       The original input text broken across multiple lines.
+   */
   public static final String text( String str, int maxWidth )
 
   {
@@ -497,22 +518,18 @@ public class FormatXML {
 
   //////////////////////////////////////////////////////////////////////////////
 
-  /** Return a string with a single new-line. <br><br>
-
-      @return
-          A string containing one new-line.
-  */
+  /** Return a string with a single new-line.
+   * 
+   */
   public static final String newLine() { return "\n"; }
 
   //////////////////////////////////////////////////////////////////////////////
 
-  /** Return a string containing a specified number of new-lines. <br><br>
-
-      @param lineCount  The number of new-lines to return.
-
-      @return
-          A string containing the requested number of new-lines.
-  */
+  /** Return a string containing a specified number of new-lines.
+   *
+   *  @param lineCount  The number of new-lines to return.
+   * 
+   */
   public static final String newLine( int lineCount )
 
   {
@@ -530,9 +547,28 @@ public class FormatXML {
 
   //////////////////////////////////////////////////////////////////////////////
 
+  /** Return a string containing one or two new-lines depending on whether 
+   *  the user wants blank lines after tags or not.
+   * 
+   */
+  public static final String newLineAfterText()
+
+  {
+
+    // If the caller wants a blank line after the text, return two new-lines.
+    if( mBlankLineAfterTag ) return "\n\n";
+    
+    // Otherwise, just return one.
+    return "\n";
+    
+  } // public newLineAfterText()
+
+
+  //////////////////////////////////////////////////////////////////////////////
+
   /** Indent by the current tab size for all subsequent calls to FormatXML
-      output functions. <br><br>
-  */
+   *  output functions.
+   */
   private static final void tab()
 
   {
@@ -551,8 +587,8 @@ public class FormatXML {
   //////////////////////////////////////////////////////////////////////////////
 
   /** Un-indent by the current tab size for all subsequent calls to FormatXML
-      output functions. <br><br>
-  */
+   *  output functions.
+   */
   private static final void untab()
 
   {
