e5d0a16a3c99d1cc10d1a762ad5ec867e956b582
jpgovaars <none@none>
2004-11-30 22:59:44 +0000

    New class to support HTML to XML conversion for indexing of web pages.
    --HG--
    extra : convert_revision : svn%3A5c944932-937e-0410-aa5c-9344c804e6c3/trunk%4079

---
+++ WEB-INF/src/org/cdlib/xtf/textIndexer/HTMLToString.java
@@ -0,0 +1,111 @@
+package org.cdlib.xtf.textIndexer;
+
+/**
+ * Copyright (c) 2004, Regents of the University of California
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, 
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice, 
+ *   this list of conditions and the following disclaimer in the documentation 
+ *   and/or other materials provided with the distribution.
+ * - Neither the name of the University of California nor the names of its
+ *   contributors may be used to endorse or promote products derived from this 
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+import java.io.FileInputStream;
+import java.io.ByteArrayOutputStream;
+import org.w3c.tidy.Tidy;
+
+import org.cdlib.xtf.util.*;
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+public class HTMLToString {
+
+  static Tidy tidy = new Tidy();  
+ 
+  //////////////////////////////////////////////////////////////////////////////
+  
+  static public String convert( String HTMLFileName )
+ 
+  {
+  
+    // Tell Tidy to make XML as it output.
+    tidy.setXmlOut( true );
+    
+    try {
+        
+        // Get hold of the source file.
+        FileInputStream in = new FileInputStream( HTMLFileName );
+        
+        // Create a buffer to output the XML to.
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        
+        // Convert the HTML to XML.
+        tidy.parse( in, out );
+        
+        // Get a string version of the resulting XML.
+        String retStr = out.toString();
+        
+        // Check to see if the XML has a document type tag.
+        int docStartIdx = retStr.indexOf( "<!DOCTYPE" );
+        
+        // If it does...
+        if( docStartIdx != -1 ) {
+
+            // Find the end of the tag.
+            int docEndIdx = retStr.indexOf( ">", docStartIdx );
+            
+            // Isolate the entire document type tag.
+            String docTypeStr = retStr.substring( docStartIdx, 
+                                                  docEndIdx-docStartIdx+1 );
+            
+            // And then remove the document tag from the XML. Why? Because
+            // it often causes trouble when the XML parser tries to interpret
+            // it. Especially coming from HTML.
+            //
+            retStr = retStr.replaceAll( docTypeStr, "" );
+        }
+        
+        // Also, XML doesn't support the "&nbsp;" alias, so replace any 
+        // occurences with the actual character code for a non-breaking
+        // space.
+        // 
+        retStr = retStr.replaceAll( "&nbsp;", "&#160;" );
+        
+        // Finally, return the XML string to the caller.
+        return retStr;
+    
+    } //try
+    
+    // If anything went wrong, say what it was.
+    catch( Throwable t ) {
+        Trace.error( "*** HTMLToXML.convert() Exception: " + t.getClass() );
+        Trace.error( "                     With message: " + t.getMessage() );
+    }
+
+    // If we got to this point, something went wrong. So return a null
+    // string back to the caller.
+    //
+    return null;
+  
+  } // static public String convert()
+
+} // class HTMLToString
