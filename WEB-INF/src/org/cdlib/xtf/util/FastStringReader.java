25c2630072a467ba679acf2b15b8afb409d42762
mhaye <none@none>
2005-02-08 23:52:55 +0000

    Added a new handy constructor.
    --HG--
    extra : convert_revision : svn%3A5c944932-937e-0410-aa5c-9344c804e6c3/trunk%40198

--- WEB-INF/src/org/cdlib/xtf/util/FastStringReader.java
+++ WEB-INF/src/org/cdlib/xtf/util/FastStringReader.java
@@ -29,6 +29,8 @@ package org.cdlib.xtf.util;
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+import java.io.IOException;
+import java.io.Reader;
 import java.io.StringReader;
 
 /**
@@ -46,6 +48,35 @@ public class FastStringReader extends StringReader
     public FastStringReader( String s ) {
         super( s );
         str = s;
+    }
+    
+    /** Wrap a normal reader with a fast string reader */
+    public FastStringReader( Reader reader ) {
+        this( readerToString(reader) );
+    }
+    
+    /** Read all the characters from a Reader, and return the resulting
+     *  concatenated string. 
+     */
+    public static String readerToString( Reader reader ) 
+    {
+        char[] ch = new char[256];
+        StringBuffer buf = new StringBuffer( 256 );
+        while( true ) {
+            try {
+                int nRead = reader.read( ch );
+                if( nRead < 0 )
+                    break;
+                buf.append( ch, 0, nRead );
+            }
+            catch( IOException e ) { 
+                // This really can't happen, given that the reader is always
+                // a StringReader. But if it does, barf out.
+                //
+                throw new RuntimeException( e ); 
+            }
+        } // while
+        return buf.toString();
     }
     
     /** Get the string back */
