2107a579691211de6d62da4d2afdfecd83e5f897
mhaye <none@none>
2005-12-20 19:16:52 +0000

    New code to correctly handle exact queries on fields that start or end with punctuation.
    --HG--
    extra : convert_revision : svn%3A5c944932-937e-0410-aa5c-9344c804e6c3/trunk%40715

--- WEB-INF/src/org/cdlib/xtf/textEngine/SpanExactQuery.java
+++ WEB-INF/src/org/cdlib/xtf/textEngine/SpanExactQuery.java
@@ -39,6 +39,7 @@ import org.apache.lucene.index.Term;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.Searcher;
 import org.apache.lucene.search.spans.SpanNearQuery;
+import org.apache.lucene.search.spans.SpanOrQuery;
 import org.apache.lucene.search.spans.SpanQuery;
 import org.apache.lucene.search.spans.SpanTermQuery;
 import org.apache.lucene.search.spans.Spans;
@@ -112,41 +113,134 @@ public class SpanExactQuery extends SpanQuery
         // Modify the first and last clauses to include the special start-of-field
         // and end-of-field markers.
         //
-        SpanQuery[] newClauses = new SpanQuery[clauses.length];
+        ArrayList newClauses = new ArrayList(clauses.length);
         for( int i = 0; i < clauses.length; i++ ) {
             if( !(clauses[i] instanceof SpanTermQuery) )
                 throw new RuntimeException( "Exact queries only support plain terms" );
             
             // We only fool with the first and last clauses
-            if( i > 0 && i < clauses.length-1 ) {
-                newClauses[i] = clauses[i];
+            boolean isFirst = (i == 0);
+            boolean isLast  = (i == clauses.length-1);
+            if( !isFirst && !isLast ) {
+                newClauses.add( clauses[i] );
                 continue;
             }
             
             // Get the term out.
             SpanTermQuery oldClause = (SpanTermQuery) clauses[i];
-            String        term      = oldClause.getTerm().text();
+            String        oldTerm   = oldClause.getTerm().text();
+            String        field     = oldClause.getTerm().field();
+
+            // Handy things we'll need later
+            SpanQuery detachedStartQuery = new SpanTermQuery( 
+                              new Term(field, ""+Constants.FIELD_START_MARKER) );            
+            SpanQuery detachedEndQuery = new SpanTermQuery( 
+                              new Term(field, ""+Constants.FIELD_END_MARKER) );
             
-            // If this is the first clause, add the start-of-field marker.
-            if( i == 0 )
-                term = Constants.FIELD_START_MARKER + term;
+            // We may need to OR up to four clauses together. Why? Consider
+            // this data:
+            //
+            //   <subject>(foo)</subject>
+            //
+            // This will be indexed with start/end markers like this:
+            //
+            //   ?(foo)?      (where ? represents the markers)
+            //
+            // Normally our exact query would just look for ?foo?, but that
+            // won't match the above data because the parentheses force the
+            // markers to be indexed as individual terms rather than as part
+            // of the word "foo". But we really want to be able to get an 
+            // exact match on data that has punctuation (and on data that 
+            // doesn't.) So we form a query that looks like this:
+            //
+            //   OR("?foo?", "? foo?", "?foo ?", "? foo ?")
+            //
+            // Note the spacing. This basically queries for all combinations
+            // of attached or detached markers. In the case of a one-term
+            // exact query, you end up with four clauses in the OR as above. 
+            // But in the case of multiple terms, like a query for "cat bar flu" 
+            // you would get:
+            //
+            //   PHRASE(OR("?cat", "? cat"), "bar", OR("flu?", "flu ?"))
+            //
+            // So following is logic that accomplishes all this magic...
+            //
+            ArrayList orClauses = new ArrayList();
             
-            // If this is the last clause, add the end-of-field marker.
-            if( i == clauses.length-1 )
-                term = term + Constants.FIELD_END_MARKER;
+            // We may need the start marker: (0) absent; (1) detached; or
+            // (2) attached.
+            //
+            for( int startAtt = 0; startAtt < 3; ++startAtt ) {
+                
+                // It can only be absent if this isn't the first token.
+                // Likewise, it can only be present if this is the first.
+                //
+                if( (startAtt == 0 && isFirst) || (startAtt != 0 && !isFirst) )
+                    continue;
+                
+                // Okay, we have the same three choices for the end marker.
+                for( int endAtt = 0; endAtt < 3; ++endAtt ) {
+                    
+                    // It can only be absent if this isn't the last token.
+                    // Likewise, it can only be present if this is the last.
+                    //
+                    if( (endAtt == 0 && isLast) || (endAtt != 0 && !isLast) )
+                      continue;
+                    
+                    // We'll form a phrase of up to three terms.
+                    ArrayList phraseClauses = new ArrayList();
+                    
+                    // First, a detached start marker.
+                    if( startAtt == 1 )
+                        phraseClauses.add( detachedStartQuery );
+                    
+                    // Next, the term itself with or without attached markers.
+                    String newTerm = oldTerm;
+                    if( startAtt == 2 )
+                        newTerm = Constants.FIELD_START_MARKER + newTerm;
+                    if( endAtt == 2 )
+                        newTerm = newTerm + Constants.FIELD_END_MARKER;
+                    
+                    SpanQuery newClause = new SpanTermQuery(
+                        new Term(field, newTerm) );
+                    newClause.setBoost( oldClause.getBoost() );
+                    phraseClauses.add( newClause );
+                    
+                    // Finally, a detached end marker.
+                    if( endAtt == 1 )
+                        phraseClauses.add( detachedEndQuery );
+                    
+                    // If only one term, skip the phrase query.
+                    int nTerms = phraseClauses.size();
+                    if( nTerms == 1 )
+                        orClauses.add( phraseClauses.get(0) );
+                    else {
+                        orClauses.add( new SpanNearQuery(
+                            (SpanQuery[])phraseClauses.toArray(new SpanQuery[nTerms]),
+                            0, true) );
+                    }
+                } // for endAtt
+            } // for startAtt
             
-            // Construct the new clause.
-            SpanTermQuery newClause = new SpanTermQuery(
-                new Term(oldClause.getTerm().field(), term),
-                oldClause.getTermLength() );
-            newClause.setBoost( oldClause.getBoost() );
-            
-            newClauses[i] = newClause;
+            // If only one clause, skip the OR
+            if( orClauses.size() == 1 ) {
+                newClauses.add( orClauses.get(0) );
+                continue;
+            }
+
+            // Make an OR to stick them together
+            SpanOrQuery orQuery = new SpanOrQuery(
+                (SpanQuery[]) orClauses.toArray(new SpanQuery[orClauses.size()]) );
+            newClauses.add( orQuery );
         } // for i
             
-        // And make a near query out of it.
-        SpanQuery q = new SpanNearQuery(newClauses, 20, true);
+        // And make a near query out of the whole thing.
+        SpanQuery q = new SpanNearQuery(
+            (SpanQuery[])newClauses.toArray(new SpanQuery[newClauses.size()]),
+            0, true);
         q.setSpanRecording( getSpanRecording() );
+        
+        // Return the spans from the rewritten query.
         return q.getSpans( reader, searcher );
     }
     
