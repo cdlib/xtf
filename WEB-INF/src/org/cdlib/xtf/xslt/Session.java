fd71fa72ca56de53da6b93ae1001340ec1b9d086
mhaye <none@none>
2006-02-25 00:45:19 +0000

    Add the ability to store and retrieve structured XML in session data. Detects whether a string or a Node is passed, and does the right thing accordingly.
    --HG--
    extra : convert_revision : svn%3A5c944932-937e-0410-aa5c-9344c804e6c3/trunk%40797

--- WEB-INF/src/org/cdlib/xtf/xslt/Session.java
+++ WEB-INF/src/org/cdlib/xtf/xslt/Session.java
@@ -29,13 +29,35 @@ package org.cdlib.xtf.xslt;
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Properties;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 
 import org.cdlib.xtf.servletBase.TextServlet;
+import org.cdlib.xtf.util.XTFSaxonErrorListener;
 
+import net.sf.saxon.FeatureKeys;
 import net.sf.saxon.expr.XPathContext;
+import net.sf.saxon.om.AllElementStripper;
+import net.sf.saxon.om.Axis;
+import net.sf.saxon.om.AxisIterator;
+import net.sf.saxon.om.Item;
+import net.sf.saxon.om.NodeInfo;
+import net.sf.saxon.tinytree.TinyBuilder;
+import net.sf.saxon.trans.DynamicError;
+import net.sf.saxon.trans.XPathException;
+import net.sf.saxon.type.Type;
+import net.sf.saxon.value.SingletonNode;
+import net.sf.saxon.value.StringValue;
+import net.sf.saxon.value.Value;
 
 /**
  * Utility functions to store and access variables in the user's session. 
@@ -53,7 +75,8 @@ public class Session
   } // isEnabled()
   
   /** Function to get a data from a session variable. */
-  public static String getData( XPathContext context, String name )
+  public static Value getData( XPathContext context, String name )
+    throws XPathException
   {
     HttpServletRequest req = TextServlet.getCurRequest();
     
@@ -65,8 +88,15 @@ public class Session
     if( val == null )
         return null;
     
-    return val;
+    return getValue( context, val );
   } // getData()
+  
+  /** Function to put structured data into a session variable. */
+  public static void setData( XPathContext context, String name, Value value )
+    throws XPathException
+  {
+    setData( context, name, getString(context, value) );
+  } // setData()
   
   /** Function to put data into a session variable. */
   public static void setData( XPathContext context, String name, String value )
@@ -82,7 +112,8 @@ public class Session
     HttpServletRequest req = TextServlet.getCurRequest();
     HttpSession session = req.getSession( true );
     String oldVal = (String) session.getAttribute( name );
-    session.setAttribute( name, value );
+    if( !value.equals(oldVal) )
+        session.setAttribute( name, value );
   } // setData()
   
   /** Function to encode a URL, adding session ID if necessary. */
@@ -92,5 +123,106 @@ public class Session
     String mappedURL = res.encodeURL( origURL );
     return mappedURL;
   } // encodeURL()
+
+  /**
+   * Gets a proper string for the value. If the value is simply a string, we
+   * return just that. If the value is some structured XML, we return 
+   * XML with a header.
+   * 
+   * @param context Context for the evaluation
+   * @return        A byte stream, properly formatted
+   */
+  public static String getString( XPathContext context, Value value ) 
+    throws XPathException
+  {
+    if( value instanceof StringValue )
+        return value.getStringValue();
+    
+    try {
+        // Convert the value to a proper NodeInfo we can examine
+        NodeInfo node = (NodeInfo) 
+            value.convertToJava( NodeInfo.class, context );
+        
+        // Detect whether there are any elements in the document.
+        int nElements = 0;
+        AxisIterator iter = node.iterateAxis( Axis.CHILD );
+        while( true ) {
+            Item kid = iter.next();
+            if( kid == null )
+                break;
+            if( kid instanceof NodeInfo && 
+                ((NodeInfo)kid).getNodeKind() == Type.ELEMENT )
+            {
+                ++nElements;
+            }
+        }
+        
+        // If no elements, get the simple string value.
+        if( nElements == 0 )
+            return value.toString();
+        else if( nElements > 1 ) {
+            DynamicError err = new DynamicError(
+                "Error converting XML to string: there must be exactly one root element" );
+            err.setXPathContext(context);
+            throw err;
+        }
+        
+        // Convert to XML.
+        StringWriter writer = new StringWriter();
+        StreamResult streamResult = new StreamResult( writer );
+        TransformerFactory factory = new net.sf.saxon.TransformerFactoryImpl();
+        
+        // Avoid NamePool translation, as it triggers a Saxon bug. 
+        factory.setAttribute( FeatureKeys.NAME_POOL, node.getNamePool() );
+        
+        Transformer trans = factory.newTransformer();
+        Properties props = trans.getOutputProperties();
+        props.put( "indent", "yes" );
+        props.put( "method", "xml" );
+        trans.setOutputProperties( props );
+        
+        // Make sure errors get directed to the right place.
+        if( !(trans.getErrorListener() instanceof XTFSaxonErrorListener) )
+            trans.setErrorListener( new XTFSaxonErrorListener() );
+
+        trans.transform( node, streamResult );
+        
+        // All done.
+        String ret = writer.getBuffer().toString();
+        return ret;
+    }
+    catch( Exception e ) {
+        DynamicError err = new DynamicError(
+            "Exception occurred converting XML to string: " + e );
+        err.setXPathContext(context);
+        throw err;
+    }
+  } // getString()
+
+  /**
+   * Checks if the input string is actually an XML document. If so, returns
+   * a value containing the parsed XML as a node. Otherwise, returns a
+   * simple string value.
+   *  
+   * @param str   The string to check.
+   * @return      Either a SingletonNode or a StringValue.
+   */
+  private static Value getValue( XPathContext context, String str )
+    throws XPathException
+  {
+    // See if we got XML.
+    int idx = str.indexOf("<?xml");
+    if( idx < 0 || idx > 10 ) 
+    {
+        // Doesn't look like XML. Just treat it as a string.
+        return new StringValue( str );
+    }
+        
+    // Ooh, we got some XML. Let's make a real Node out of it.
+    StreamSource src = new StreamSource( new StringReader(str) );
+    NodeInfo doc = TinyBuilder.build( src, new AllElementStripper(), 
+                          context.getController().getConfiguration() );
+    return new SingletonNode( doc );
+  } // getValue()
 
 } // class Session
