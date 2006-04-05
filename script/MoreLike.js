a5682e2a91fa4137a3e23d5268005cb82ead1d34
mhaye <none@none>
2006-04-05 21:30:08 +0000

    Lots of parameters for 'more like this' query.
    --HG--
    extra : convert_revision : svn%3A5c944932-937e-0410-aa5c-9344c804e6c3/trunk%40875

--- script/MoreLike.js
+++ script/MoreLike.js
@@ -5,10 +5,53 @@
 //
 //////////////////////////////////////////////////////////////////////////////
 
-// Asynchronously fetch more docs
+// If the form with additional MoreLike parameters is present, add a parameter
+// to the URL.
+//
+addTextParam = function( ark, name, url )
+{
+  var textElement = document.getElementById( ark + "-" + name );
+  if( textElement == null )
+      return url;
+  else
+      return url + "&" + name + "=" + textElement.value;
+}
+
+addFieldsParam = function( ark, url )
+{
+  var fields = "";
+  for( var i = 1; ; i++ ) {
+      var element = document.getElementById( ark + "-field-" + i );
+      if( element == null )
+          break;
+      if( element.value == "0" || element.value == "0.0" )
+          continue;
+      if( i > 1 )
+          fields += ",";
+      fields += element.name;
+      if( element.value != "1" && element.value != "1.0" )
+          fields += "^" + element.value;
+  }
+  if( i == 1 )
+      return url;
+  else
+      return url + "&fieldsToScan=" + fields;   
+}
+    
+// Asynchronously fetch more docs "like this".
 moreLike = function( ark, url )
 {
+  url = addTextParam( ark, "minWordLen", url );
+  url = addTextParam( ark, "maxWordLen", url );
+  url = addTextParam( ark, "minDocFreq", url );
+  url = addTextParam( ark, "maxDocFreq", url );
+  url = addTextParam( ark, "minTermFreq", url );
+  url = addTextParam( ark, "termBoost", url );
+  url = addTextParam( ark, "maxQueryTerms", url );
+  url = addFieldsParam( ark, url );
+
   var element = document.getElementById( ark + "-moreLike" );
+  //element.innerHTML = url; return;
   element.innerHTML = "Fetching...";
   var loader = new net.AsyncLoader( url, 
       function() { element.innerHTML = this.req.responseText; },
