a883cc1cb577250b5fc35b1eb2e0e741f4002300
mhaye <none@none>
2006-07-15 00:49:38 +0000

    Very fast array-based lists of common built-in types.
    --HG--
    extra : convert_revision : svn%3A5c944932-937e-0410-aa5c-9344c804e6c3/trunk%40974

---
+++ WEB-INF/src/org/cdlib/xtf/util/ArrayUtil.java
@@ -0,0 +1,75 @@
+package org.cdlib.xtf.util;
+
+/**
+ * Copyright (c) 2006, Regents of the University of California
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, 
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice, 
+ *   this list of conditions and the following disclaimer in the documentation 
+ *   and/or other materials provided with the distribution.
+ * - Neither the name of the University of California nor the names of its
+ *   contributors may be used to endorse or promote products derived from this 
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * Various handy functions for working with arrays.
+ * 
+ * @author Martin Haye
+ */
+public class ArrayUtil
+{
+  // Functions for int arrays
+    
+  public static int[] resize( int[] in, int newSize ) {
+      int[] out = new int[newSize];
+      System.arraycopy( in, 0, out, 0, Math.min(in.length, newSize) );
+      return out;
+  }
+  
+  public static int[] expand( int[] in ) {
+      return resize( in, in.length * 3 / 2 );
+  }
+  
+  // Functions for float arrays
+  
+  public static float[] resize( float[] in, int newSize ) {
+      float[] out = new float[newSize];
+      System.arraycopy( in, 0, out, 0, Math.min(in.length, newSize) );
+      return out;
+  }
+  
+  public static float[] expand( float[] in ) {
+      return resize( in, in.length * 3 / 2 );
+  }
+  
+  // Functions for String arrays
+  
+  public static String[] resize( String[] in, int newSize ) {
+      String[] out = new String[newSize];
+      System.arraycopy( in, 0, out, 0, Math.min(in.length, newSize) );
+      return out;
+  }
+  
+  public static String[] expand( String[] in ) {
+      return resize( in, in.length * 3 / 2 );
+  }
+  
+} // class ArrayUtil
---
+++ WEB-INF/src/org/cdlib/xtf/util/StringList.java
@@ -0,0 +1,87 @@
+package org.cdlib.xtf.util;
+
+import java.util.Arrays;
+
+/**
+ * Copyright (c) 2006, Regents of the University of California
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, 
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice, 
+ *   this list of conditions and the following disclaimer in the documentation 
+ *   and/or other materials provided with the distribution.
+ * - Neither the name of the University of California nor the names of its
+ *   contributors may be used to endorse or promote products derived from this 
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+// A fast, array-based, expandable list of ints
+public class StringList
+{
+  private String[] data;
+  private int   size = 0;
+  
+  public StringList() {
+      this( 10 );
+  }
+  
+  public StringList( int initialCapacity ) {
+      data = new String[initialCapacity];
+  }
+  
+  public final void add( String value ) {
+      if( size == data.length )
+          data = ArrayUtil.expand( data );
+      data[size++] = value;
+  }
+  
+  public final void compact() {
+      if( size != data.length )
+          data = ArrayUtil.resize( data, size );
+  }
+  
+  public final boolean isEmpty() { return size == 0; }
+  
+  public final void clear() { size = 0; }
+  
+  public final int size() { return size; }
+  
+  public final String get(int index) { return data[index]; }
+  
+  public final String getLast() { return data[size-1]; }
+  
+  public final void set( int index, String value ) {
+      data[index] = value;
+  }
+  
+  public final void fill( String value ) {
+      Arrays.fill( data, value );
+  }
+  
+  public final void sort() {
+      compact();
+      Arrays.sort( data );
+  }
+  
+  public final int binarySearch( String searchFor ) {
+      compact();
+      return Arrays.binarySearch( data, searchFor );
+  }
+  
+} // class StringList
