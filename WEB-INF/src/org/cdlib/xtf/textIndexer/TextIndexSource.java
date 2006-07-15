fdaf388e497a36d977b2bd2f927835ba2f5514d9
mhaye <none@none>
2006-07-15 00:50:55 +0000

    Removed unused import.
    --HG--
    extra : convert_revision : svn%3A5c944932-937e-0410-aa5c-9344c804e6c3/trunk%40976

--- WEB-INF/src/org/cdlib/xtf/textIndexer/TextIndexSource.java
+++ WEB-INF/src/org/cdlib/xtf/textIndexer/TextIndexSource.java
@@ -32,7 +32,6 @@ package org.cdlib.xtf.textIndexer;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
