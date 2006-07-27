2ce49decc5f9f13f555c83eb6ae779c39813b837
mhaye <none@none>
2006-07-27 02:34:15 +0000

    Increased buffer size for more efficient file copies.
    --HG--
    extra : convert_revision : svn%3A5c944932-937e-0410-aa5c-9344c804e6c3/trunk%401000

--- WEB-INF/src/org/cdlib/xtf/util/Path.java
+++ WEB-INF/src/org/cdlib/xtf/util/Path.java
@@ -443,7 +443,7 @@ public class Path
       OutputStream out = new FileOutputStream( dst );
   
       // Transfer bytes from in to out
-      byte[] buf = new byte[1024];
+      byte[] buf = new byte[(int) Math.min(src.length(), 1024*256)];
       int len;
       while( (len = in.read(buf)) > 0 )
           out.write(buf, 0, len);
