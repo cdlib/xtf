899bdd9e727b0eebf72a9dc9c1e726c60f4bae8b
mhaye <none@none>
2006-08-16 23:35:28 +0000

    Support for coordinated sorting.
    --HG--
    extra : convert_revision : svn%3A5c944932-937e-0410-aa5c-9344c804e6c3/trunk%401061

--- WEB-INF/src/org/cdlib/xtf/util/IntList.java
+++ WEB-INF/src/org/cdlib/xtf/util/IntList.java
@@ -1,6 +1,7 @@
 package org.cdlib.xtf.util;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Copyright (c) 2006, Regents of the University of California
@@ -90,4 +91,92 @@ public class IntList
       return Arrays.binarySearch( data, searchFor );
   }
   
+  public final int[] calcSortMap() 
+  {
+    int i;
+    
+    // First, form a mapping representing the current order.
+    int[] map = new int[size];
+    for( i = 0; i < size; i++ ) {
+        if( data[i] < 0 )
+            throw new RuntimeException( "This radix-sort cannot handle negative numbers" );
+        map[i] = i;
+    }
+    
+    // Now perform a radix sort by bytes
+    final int     MAX_BITS = 32;
+    final short[] curByte  = new short[size];
+    final int[]   count    = new int[256];
+    int[]         newMap   = new int[size];
+    for( int shift = 0; shift < MAX_BITS; shift += 8)
+    {
+        // Count the size of each bucket.
+        Arrays.fill( count, 0 );
+        for( i = 0; i < size; i++ )
+           ++count[ curByte[i] = (short) ((data[map[i]] >> shift) & 0xFF) ];
+        
+        // Calculate the offset of each bucket
+        int pos = 0;
+        for( i = 0; i < 256; i++ ) {
+            int tmp = pos;
+            pos += count[i];
+            count[i] = tmp;
+        }
+        
+        // Compute the new mapping.
+        for( i = 0; i < size; i++ )
+            newMap[count[curByte[i]]++] = map[i];
+        
+        // Flip and prepare for next pass.
+        int[] tmp = map;
+        map = newMap;
+        newMap = tmp;
+        
+    } // for shift
+    
+    // Sorting algorithms are famous for bugs. Make a check pass to ensure that
+    // we didn't mess up.
+    //
+    int prev = Integer.MIN_VALUE;
+    for( i = 0; i < size; i++ ) {
+        int tmp = data[map[i]];
+        if( tmp < prev )
+            throw new RuntimeException( "Fatal internal error: sort algorithm has a bug" );
+        prev = tmp;
+    }
+    
+    // Return the final mapping.
+    return map;
+  } // calcSortMapping()
+  
+  public final void remap( int[] map )
+  {
+    int[] newData = new int[size];
+    assert map.length == size;
+    for( int i = 0; i < size; i++ )
+        newData[i] = data[map[i]];
+    
+    data = newData;
+  }
+  
+  /**
+   * Basic regression test
+   */
+  public static final Tester tester = new Tester("IntList") {
+    protected void testImpl()
+    {
+      IntList list = new IntList();
+      final int SIZE = 100000;
+      Random rand = new Random(1);
+      for( int i = 0; i < SIZE; i++ )
+          list.add( Math.abs(rand.nextInt()) );
+      
+      int[] map = list.calcSortMap();
+      list.remap( map );
+      
+      for( int i = 1; i < SIZE; i++)
+          assert list.get(i-1) <= list.get(i);
+      
+    } // testImpl()
+  };
 } // class IntList
