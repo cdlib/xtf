6e2cc0b084ea5ef360cf2958004a48f74b87688b
mhaye <none@none>
2007-03-19 18:20:44 +0000

    Ensure that non-numeric progress message changes always get output (eventually).
    --HG--
    extra : convert_revision : svn%3A5c944932-937e-0410-aa5c-9344c804e6c3/trunk%401299

--- WEB-INF/contrib/xtf-lucene/src/java/org/apache/lucene/util/ProgressTracker.java
+++ WEB-INF/contrib/xtf-lucene/src/java/org/apache/lucene/util/ProgressTracker.java
@@ -30,6 +30,8 @@ public abstract class ProgressTracker implements Cloneable
   private IntHolder prevPctDone = new IntHolder();
   private LongHolder prevTime = new LongHolder();
   private StringHolder prevDescrip = new StringHolder();
+  private IntHolder skippedPctDone = new IntHolder();
+  private StringHolder skippedDescrip = new StringHolder();
 
   /** Initialize a 0..100% tracker */
   public ProgressTracker() {
@@ -119,11 +121,26 @@ public abstract class ProgressTracker implements Cloneable
     if (pctDone != prevPctDone.value ||
         (pctDone == 100 && !descrip.equals(prevDescrip.value))) 
     {
-      // Observe the stated minimum interval for updates (except for the last one)
+      // Observe the stated minimum interval for updates, unless the 
+      // non-numeric part of the message has changed.
+      //
+      boolean nonNumChg = nonNumericChange(descrip, prevDescrip.value);
       if (pctDone < 100 &&
+          !nonNumChg &&
           System.currentTimeMillis() - prevTime.value < minInterval)
+      {
+        if (!descrip.equals(prevDescrip.value)) { 
+          skippedPctDone.value = pctDone;
+          skippedDescrip.value = descrip;
+        }
         return;
-
+      }
+      
+      // If there is a non-numeric change and we skipped one, output it now.
+      if (nonNumChg && skippedDescrip.value != null)
+        report(skippedPctDone.value, skippedDescrip.value);
+      skippedDescrip.value = null;
+      
       // Report the progress (but not the same description twice)
       report(pctDone, descrip.equals(prevDescrip.value) ? "" : descrip);
 
@@ -132,6 +149,32 @@ public abstract class ProgressTracker implements Cloneable
       prevPctDone.value = pctDone;
       prevDescrip.value = descrip;
     }
+  }
+  
+  private boolean nonNumericChange(String s1, String s2)
+  {
+    if (s1 == null || s2 == null)
+      return true;
+    
+    int p1 = 0;
+    int p2 = 0;
+    while (p1 < s1.length() && p2 < s2.length()) {
+      char c1 = s1.charAt(p1);
+      char c2 = s2.charAt(p2);
+      if (Character.isDigit(c1))
+        p1++;
+      else if (Character.isDigit(c2))
+        p2++;
+      else if (c1 != c2)
+        return true;
+      else {
+        p1++;
+        p2++;
+      }
+    }
+    if (p1 == s1.length() && p2 == s2.length())
+      return false;
+    return true;
   }
 
   /** Supply this method to actually print out the progress */
