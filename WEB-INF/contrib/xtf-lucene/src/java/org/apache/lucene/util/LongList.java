3ee4054fcfd7de97afa80c094bb2eb717c4903d9
mhaye <none@none>
2007-03-27 01:06:39 +0000

    Added JavaDoc comments for all methods. Moved ArrayUtil back to XTF, to make IntList and LongList more independent.
    --HG--
    extra : convert_revision : svn%3A5c944932-937e-0410-aa5c-9344c804e6c3/trunk%401315

--- WEB-INF/contrib/xtf-lucene/src/java/org/apache/lucene/util/LongList.java
+++ WEB-INF/contrib/xtf-lucene/src/java/org/apache/lucene/util/LongList.java
@@ -28,84 +28,122 @@ public class LongList
   private long[] data;
   private int size = 0;
 
+  /** Basic constructor - initializes with capacity of 10 */
   public LongList() {
     this(10);
   }
 
+  /** Constructor to specify initial capacity explicitly */
   public LongList(int initialCapacity) {
     data = new long[initialCapacity];
   }
 
+  /** Add a value to the end of the list, expanding the array if necessary */
   public final void add(long value) {
     if (size == data.length)
-      data = ArrayUtil.expand(data);
+      resize(size * 3 / 2);
     data[size++] = value;
   }
 
+  /** Ensure that at least the given number of elements can be stored */
   public final void ensureCapacity(int cap) {
     if (cap > data.length)
-      data = ArrayUtil.resize(data, cap);
+      resize(cap);
   }
 
+  /** Resize the array so it exactly fits the current elements */
   public final void compact() {
     if (size != data.length)
-      data = ArrayUtil.resize(data, size);
+      resize(size);
   }
 
+  /** 
+   * Resize the array to the specified size. If smaller than the current
+   * number of elements, the ones at the end will be lost.
+   */
   public final void resize(int newSize) 
   {
     if (newSize != size) {
-      data = ArrayUtil.resize(data, newSize);
-      if (newSize > size)
-        Arrays.fill(data, size, newSize, 0L);
-      size = newSize;
+      long[] oldData = data;
+      data = new long[newSize];
+      System.arraycopy(oldData, 0, data, 0, Math.min(size, newSize));
+      size = Math.min(size, newSize);
     }
   }
 
+  /** 
+   * Get an array of the elements. This is a copy, so it's safe to modify
+   * without disturbing the contents of the list. 
+   */
   public final long[] toArray() {
     long[] ret = new long[size];
     System.arraycopy(data, 0, ret, 0, size);
     return ret;
   }
 
+  /** Check if the list is empty (i.e. size() == 0) */
   public final boolean isEmpty() {
     return size == 0;
   }
 
+  /** Remove all elements from the list (but doesn't resize the array) */
   public final void clear() {
     size = 0;
   }
 
+  /** Retrieve the current number of elements in the list */
   public final int size() {
     return size;
   }
 
+  /** Get an element from the list */
   public final long get(int index) {
     return data[index];
   }
 
+  /** Get the last element from the list */
   public final long getLast() {
     return data[size - 1];
   }
 
+  /** Set an element in the list */
   public final void set(int index, long value) {
     data[index] = value;
   }
 
+  /** Fill the list with a given data value */
   public final void fill(long value) {
     Arrays.fill(data, value);
   }
 
+  /** Sort all the elements in the list in ascending order */
   public final void sort() {
     compact();
     Arrays.sort(data);
   }
 
+  /** 
+   * Perform a binary search for the given value. Note that the list must
+   * already be in ascending sorted order for this to work correctly; call
+   * {@link #sort()} if necessary.
+   * 
+   * @return    Same as for {@link Arrays#binarySearch(int[], int)}
+   */
   public final int binarySearch(long searchFor) {
     compact();
     return Arrays.binarySearch(data, searchFor);
   }
 
+  /**
+   * Calculate a reordering of the elements of the list that would put them
+   * in sorted order. Useful for maintaining multiple lists, and sorting them
+   * by a primary key; this is done by calling this method to get the mapping,
+   * then calling {@link #remap(int[])} on each array including the original.
+   * 
+   * @return    An array of the same size as the number of elements. Each
+   *            element of the array specifies the position that corresponding
+   *            element should be placed in a sorted array.
+   */
   public final int[] calcSortMap() 
   {
     int i;
@@ -165,6 +203,7 @@ public class LongList
     return map;
   } // calcSortMapping()
 
+  /** Apply a sort order to the elements -- see {@link #calcSortMap()} */
   public final void remap(int[] map) 
   {
     long[] newData = new long[size];
@@ -174,25 +213,4 @@ public class LongList
 
     data = newData;
   }
-
-  /**
-   * Basic regression test
-   */
-//  public static final Tester tester = new Tester("LongList") 
-//  {
-//    protected void testImpl() 
-//    {
-//      LongList list = new LongList();
-//      final int SIZE = 100000;
-//      Random rand = new Random(1);
-//      for (int i = 0; i < SIZE; i++)
-//        list.add(Math.abs(rand.nextLong()));
-//
-//      int[] map = list.calcSortMap();
-//      list.remap(map);
-//
-//      for (int i = 1; i < SIZE; i++)
-//        assert list.get(i - 1) <= list.get(i);
-//    } // testImpl()
-//  };
-} // class LongList
+}
