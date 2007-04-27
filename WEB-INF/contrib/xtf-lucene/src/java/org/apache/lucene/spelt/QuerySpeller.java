1fee6ab3df9e32fba152d96f8423b129554ce878
mhaye <none@none>
2007-04-27 22:56:42 +0000

    Handle the common case of analyzers that convert tokens to lowercase. We need to parse the query ourselves, ensuring that a non-case-converting analyzer is used.
    --HG--
    extra : convert_revision : svn%3A5c944932-937e-0410-aa5c-9344c804e6c3/trunk%401359

--- WEB-INF/contrib/xtf-lucene/src/java/org/apache/lucene/spelt/QuerySpeller.java
+++ WEB-INF/contrib/xtf-lucene/src/java/org/apache/lucene/spelt/QuerySpeller.java
@@ -18,18 +18,30 @@ package org.apache.lucene.spelt;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.Token;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.standard.StandardFilter;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.Query;
 
+// TODO: QuerySpeller javadoc
 class QuerySpeller extends SimpleQueryRewriter
 {
   /** Used to get spelling suggestions */
   private SpellReader spellReader;
+
+  /** Used to tokenize queries */
+  private Analyzer analyzer;
   
   /** Set of fields we're allowed to collect terms for */
   private HashSet<String> fieldSet;
@@ -39,29 +51,83 @@ class QuerySpeller extends SimpleQueryRewriter
   
   /** Mapping of terms to replace */
   private HashMap<String, String> suggestMap;
+
+  /** Used to parse queries */
+  private QueryParser queryParser;
       
   /** 
-   * Construct a new speller using a given dictionary reader.
+   * Construct a new speller using a given dictionary reader. The queries
+   * will be parsed with a {@link MinimalAnalyzer}, and the default field
+   * name will be "text".
    *
    * @param spellReader source for spelling suggestions -- see
    *                    {@link SpellReader#open(File)}.
    */
   public QuerySpeller(SpellReader spellReader)
   {
+    this(spellReader, new QueryParser("text", new MinimalAnalyzer()));
+  }
+  
+  /**
+   * Construct a new speller using a given dictionary reader and analyzer (note
+   * that the analyzer should do MINIMAL token filtering, without any case
+   * conversion).
+   * 
+   * @param spellReader
+   *          source for spelling suggestions -- see {@link SpellReader#open(File)}.
+   * @param minimalQueryParser
+   *          used to parse queries; note that the analyzer it uses should do
+   *          only MINIMAL token filtering, not even conversion to lower case,
+   *          so that suggestions can be made in the same case the user typed
+   *          them. In particular, StandardAnalyzer should not be used.
+   */
+  public QuerySpeller(SpellReader spellReader, 
+                      QueryParser minimalQueryParser)
+  {
     this.spellReader = spellReader;
+    this.queryParser = minimalQueryParser;
+    
+    // Test out the query parser's analyzer to make sure it preserves
+    // the case of input tokens.
+    //
+    validateAnalyzer();
+  }
+  
+  /** 
+   * Make sure the analyzer preserves the case of input tokens. If it didn't,
+   * we would be unable to make spelling suggestions that match the case of
+   * user queries.
+   */
+  private void validateAnalyzer()
+  {
+    TokenStream toks = queryParser.getAnalyzer().tokenStream(
+        queryParser.getField(), new StringReader("MixedCaseToken"));
+    
+    try {
+      Token t;
+      while ((t = toks.next()) != null) {
+        if (t.termText().equals("MixedCaseToken"))
+          return;
+      }
+    }
+    catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    throw new IllegalArgumentException(
+        "Unacceptable analyzer passed to QuerySpeller - must not convert to lower case");
   }
   
   /**
    * Suggest alternate spellings for terms in a Lucene query. By default,
    * we consider terms in any field. If you need to specify a subset of fields 
    * to consider, use the 
-   * {@linkplain #suggest(Query, String[]) alternate method} below.
+   * {@linkplain #suggest(String, String[]) alternate method} below.
    * 
    * @param     inQuery the original query to scan
    * @return    an query with some suggested spelling corrections, or
    *            null if no suggestions could be found. 
    */
-  public synchronized Query suggest(Query inQuery) 
+  public synchronized String suggest(String inQuery) 
     throws ParseException, IOException
   {
     return suggest(inQuery, null);
@@ -75,7 +141,7 @@ class QuerySpeller extends SimpleQueryRewriter
    * @return    a query with some suggested spelling corrections, or
    *            null if no suggestions could be found. 
    */
-  public synchronized Query suggest(Query inQuery, String[] fields) 
+  public synchronized String suggest(String inQuery, String[] fields) 
     throws ParseException, IOException
   {
     // Record the set of fields to consider.
@@ -87,9 +153,13 @@ class QuerySpeller extends SimpleQueryRewriter
         fieldSet.add(f);
     }
       
-    // Okay, traverse the query once, but don't make any changes.
+    // Okay, traverse the query once, but don't make any changes. Just collect
+    // the terms.
+    //
+    Query inQueryParsed = queryParser.parse(inQuery); 
     suggestMap = new HashMap<String, String>();
-    rewriteQuery(inQuery);
+    terms = new LinkedHashSet<String>();
+    rewriteQuery(inQueryParsed);
     
     // No terms found? Then we can't make a suggestion.
     if (terms.isEmpty())
@@ -106,15 +176,17 @@ class QuerySpeller extends SimpleQueryRewriter
       suggestMap.put(oldTerms[i], suggTerms[i]);
     
     // Rewrite the query, replacing the suggested words.
-    return rewriteQuery(inQuery);
+    Query rewritten = rewriteQuery(inQueryParsed);
+    
+    // Finally, convert the query back to a string, and we're done.
+    return rewritten.toString(queryParser.getField());
   }
   
   /** This is the way we slip in to grab or rewrite terms */
-  @Override
-  protected Term rewrite(Term t) 
+  protected @Override Term rewrite(Term t) 
   {
     // Skip fields we're not supposed to look at.
-    if (fieldSet == null || !fieldSet.contains(t.field()))
+    if (fieldSet != null && !fieldSet.contains(t.field()))
       return t;
     
     // Add this term to our accumulating list (if it's not already there)
@@ -122,10 +194,27 @@ class QuerySpeller extends SimpleQueryRewriter
     terms.add(text);
     
     // If there's a suggestion, implement it.
-    String suggText = suggestMap.get(text);
-    if (suggText != null)
-      return new Term(suggText, t.field());
+    if (suggestMap.containsKey(text)) 
+    {
+      String suggText = suggestMap.get(text);
+      if (suggText != null)
+        return new Term(t.field(), suggText);
+      else
+        return null;
+    }
     else
       return t;
+  }
+
+  /** 
+   * Performs minimal token processing, without case conversion. 
+   */
+  public static class MinimalAnalyzer extends Analyzer
+  {
+    public TokenStream tokenStream(String fieldName, Reader reader) {
+      TokenStream result = new StandardTokenizer(reader);
+      result = new StandardFilter(result);
+      return result;
+    }
   }
 }
