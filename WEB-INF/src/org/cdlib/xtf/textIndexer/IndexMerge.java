49b3e6af5bbfa954f2472ae6724e01cc50b6ede3
mhaye <none@none>
2007-05-03 22:05:33 +0000

    Bumped version numbers to 2.0
    --HG--
    extra : convert_revision : svn%3A5c944932-937e-0410-aa5c-9344c804e6c3/trunk%401389

--- WEB-INF/src/org/cdlib/xtf/textIndexer/IndexMerge.java
+++ WEB-INF/src/org/cdlib/xtf/textIndexer/IndexMerge.java
@@ -77,7 +77,7 @@ public class IndexMerge
    */
   public static void main(String[] args) 
   {
-    Trace.info("IndexMerge v. 1.9");
+    Trace.info("IndexMerge v. 2.0");
 
     try 
     {
--- WEB-INF/src/org/cdlib/xtf/textIndexer/IndexStats.java
+++ WEB-INF/src/org/cdlib/xtf/textIndexer/IndexStats.java
@@ -85,7 +85,7 @@ public class IndexStats
       boolean showUsage = false;
 
       // Regardless of whether we succeed or fail, say our name.
-      Trace.info("IndexStats v" + 1.9);
+      Trace.info("IndexStats v" + 2.0);
       Trace.info("");
 
       // Make sure the XTF_HOME environment variable is specified.
