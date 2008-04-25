b312de1c26e48b510c0e2af94824e775db54eb3e
mhaye <none@none>
2008-04-25 20:39:52 +0000

    Removed unused import.
    --HG--
    extra : convert_revision : svn%3A5c944932-937e-0410-aa5c-9344c804e6c3/trunk%401720

--- WEB-INF/src/org/cdlib/xtf/textIndexer/XMLConfigParser.java
+++ WEB-INF/src/org/cdlib/xtf/textIndexer/XMLConfigParser.java
@@ -37,7 +37,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.URI;
 import java.util.zip.GZIPInputStream;
 
 // import javax.xml.parsers.ParserConfigurationException;
