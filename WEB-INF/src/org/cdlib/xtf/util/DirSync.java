807fcdaed9b132da43a050c87006a6bfb3f4028e
Martin Haye <martin.haye@ucop.edu>
2010-11-24 13:27:26 -0800

    Switching index rotation syncing code to use rsync instead of Perl, both for speed and to avoid crashing.
    --HG--
    branch : index-rotation-rsync

--- WEB-INF/src/org/cdlib/xtf/util/DirSync.java
+++ WEB-INF/src/org/cdlib/xtf/util/DirSync.java
@@ -3,8 +3,7 @@ package org.cdlib.xtf.util;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.List;
 
 import org.cdlib.xtf.util.ProcessRunner.CommandFailedException;
 
@@ -38,15 +37,16 @@ import org.cdlib.xtf.util.ProcessRunner.CommandFailedException;
  */
 
 /**
- * Routines to synchronize one directory hierarchy to match another.
+ * Routines to synchronize one directory hierarchy to match another. Now uses
+ * rsync for speed and simplicity, and adds a threshold above which we avoid
+ * per-subdirectory syncing and just do the whole thing.
  *
  * @author Martin Haye
  */
 public class DirSync 
 {
-  private StringBuffer linkCmds;
-  private ArrayList<File> fixDirs = new ArrayList();
-  private ArrayList<Long> fixTimes = new ArrayList();
+  private static final int MAX_SELECTIVE_SYNC = 500;
+  private static final int MAX_RSYNC_BATCH = 2;
   private SubDirFilter filter;
 
   /**
@@ -75,9 +75,15 @@ public class DirSync
   public void syncDirs(File srcDir, File dstDir) 
     throws IOException
   {
-    linkCmds = new StringBuffer();
-    syncDirs(srcDir, dstDir, 0);
-    flushLinks();
+    // If there are no directories specified, or there are too many,
+    // just rsync the entire source to the dest.
+    //
+    if (filter == null || filter.size() > MAX_SELECTIVE_SYNC)
+      runRsync(srcDir, dstDir, null, null);
+    
+    // Otherwise do a selective sync.
+    else
+      selectiveSync(srcDir, dstDir);
   }
   
   /**
@@ -85,173 +91,94 @@ public class DirSync
    * 
    * @param srcDir      Directory to match
    * @param dstDir      Directory to modify
-   * @param level       0,1,2... during recursive scan
+   * @param subDirs     Sub-directories to rsync
    * @throws IOException If anything goes wrong
    */
-  private void syncDirs(File srcDir, File dstDir, int level) throws IOException
+  private void selectiveSync(File srcDir, File dstDir) 
+    throws IOException
   {
-    String[] srcList = readSortedDir(srcDir);
-    String[] dstList = readSortedDir(dstDir);
-    int srcEnd = srcList.length;
-    int dstEnd = dstList.length;
-    int s = 0;
-    int d = 0;
-    while (s < srcEnd || d < dstEnd)
+    // First, sync the top-level files (no sub-dirs)
+    runRsync(srcDir, dstDir, null, new String[] { "--exclude=/*/" });
+    
+    // Now sync the subdirectories in batches, not to exceed the batch limit
+    if (!filter.isEmpty())
     {
-      File srcFile = null;
-      File dstFile = null;
-      long direction; // if < 0 copy-src; if == 0 skip; if > 0 delete-dst
-      
-      // Decide what to do. First, if we still have source files...
-      if (s < srcEnd) 
+      ArrayList<String> dirBatch = new ArrayList();
+      String basePath = srcDir.getCanonicalPath() + "/";
+      for (String target : filter.getTargets()) 
       {
-        srcFile = new File(srcDir, srcList[s]);
+        String targetPath = new File(target).getCanonicalPath();
+        assert targetPath.startsWith(basePath);
+        targetPath = targetPath.substring(basePath.length());
         
-        // And we still have dest files...
-        if (d < dstEnd) 
-        {
-          dstFile = new File(dstDir, dstList[d]);
-          
-          // Compare the name. If no match, do the lesser first
-          direction = srcList[s].compareTo(dstList[d]);
-          
-          // If same name...
-          if (direction == 0)
-          {
-            // For directories, keep scanning down. 
-            if (srcFile.isDirectory() && dstFile.isDirectory())
-              direction = 0;
-            else 
-            {
-              // For files, delete dest if older than src; skip if same mod time.
-              direction = srcFile.lastModified() - dstFile.lastModified();
-              
-              // Special case: skip if source older than dest.
-              if (direction < 0)
-                direction = 0;
-            }
-          }
+        dirBatch.add(targetPath);
+        if (dirBatch.size() >= MAX_RSYNC_BATCH) {
+          runRsync(srcDir, dstDir, dirBatch, null);
+          dirBatch.clear();
         }
-        // Ran out of dest files. Copy src.
-        else
-          direction = -1;
       }
-      // Ran out of src files. Copy dst.
-      else {
-        dstFile = new File(dstDir, dstList[d]);
-        direction = 1;
-      }
-
-      // Now act on the decision made above.
-      if (direction < 0) {
-        File newFile = new File(dstDir, srcList[s]);
-        if (srcFile.isDirectory())
-          copyDir(srcFile, newFile, level+1);
-        else
-          copyFile(srcFile, newFile);
-        s++;
-      }
-      else if (direction > 0) {
-        if (dstFile.isDirectory())
-          deleteDir(dstFile);
-        else
-          deleteFile(dstFile);
-        d++;
-      }
-      else {
-        if (srcFile.isDirectory())
-          copyDir(srcFile, dstFile, level+1);
-        else
-          skipFile(srcFile, dstFile);
-        s++;
-        d++;
-      }
+      
+      // Finish the last batch of subdirs (if any)
+      if (!dirBatch.isEmpty())
+        runRsync(srcDir, dstDir, dirBatch, null);
     }
   }
   
-  private void skipFile(File srcFile, File dstFile) {
-    //System.out.format("Skip file %s == %s\n", srcFile.toString(), dstFile.toString());
-  }
-
-  private void deleteFile(File file) throws IOException {
-    //System.out.format("Delete file %s\n", file.toString());
-    if (!file.delete())
-      throw new IOException("Sync error: cannot delete file '" + file.toString() + "'");
-  }
-
-  private void deleteDir(File dir) throws IOException {
-    //System.out.format("Delete dir %s\n", dir.toString());
-    if (filter == null || filter.approve(dir.toString()))
-      Path.deleteDir(dir);
-  }
-
-  private void copyFile(File srcFile, File dstFile) {
-    //System.out.format("Copy file %s to %s\n", srcFile.toString(), dstFile.toString());
-    linkCmds.append("link('" + srcFile.toString() + "', '" + dstFile.toString() + "') or die;\n");
-  }
-
-  private void copyDir(File srcDir, File dstDir, int level) throws IOException {
-    //System.out.format("Copy dir %s to %s\n", srcDir.toString(), dstDir.toString());
-    if (filter == null || filter.approve(srcDir.toString())) {
-      if (!dstDir.exists() && !dstDir.mkdir())
-        throw new IOException("Error creating directory '" + dstDir + "'");
-      syncDirs(srcDir, dstDir, level);
-      if (srcDir.lastModified() != dstDir.lastModified()) {
-        fixDirs.add(dstDir);
-        fixTimes.add(srcDir.lastModified());
-      }
-    }
-    //else
-    //  System.out.format("Skip dir %s\n", srcDir.toString());
-  }
-
   /**
-   * Create all the hard links that we determined need to be made. Since
-   * Java (as of 1.6) has no ability to create these, we call out and have
-   * Perl do it for us.
-   * 
-   * @throws IOException    If something goes wrong.
+   * Run an rsync command with the standard arguments plus the
+   * specified subdirectories.
+   *
+   * @param srcDir       Directory to match
+   * @param dstDir       Directory to modify
+   * @param subDirs      Sub-directories to rsync (null for all)
+   * @throws IOException If anything goes wrong
    */
-  private void flushLinks() throws IOException 
+  public void runRsync(File srcDir, File dstDir, 
+                       List<String> subDirs,
+                       String[] extraArgs) 
+    throws IOException
   {
-    // No links? Nothing to do.
-    if (linkCmds.length() == 0)
-      return;
-    
-    // Fire up Perl and run the script to make the links.
-    try {
-      String[] args = new String[1];
-      args[0] = "perl";
-      //System.out.println("Link cmds:\n" + linkCmds.toString());
-      ProcessRunner.runAndGrab(args, linkCmds.toString(), 0);
-    } catch (InterruptedException e) {
+    try 
+    {
+      // First the basic arguments
+      ArrayList<String> args = new ArrayList(6);
+      args.add("rsync");
+      args.add("-av");
+      //args.add("--dry-run");
+      args.add("--delete");
+      
+      // Add any extra arguments at this point, before the paths.
+      if (extraArgs != null) {
+        for (String extra : extraArgs)
+          args.add(extra);
+      }
+
+      // We want to hard link dest files to the source
+      args.add("--link-dest=" + srcDir.getAbsolutePath() + "/");
+      
+      // For the source, add in the weird "./" syntax for relative syncing, e.g.
+      // rsync --relative server.org:data/13030/pairtree_root/qt/00/./{01/d5,04/k4} data/13030/pairtree_root/qt/00/
+      //
+      if (subDirs != null) { 
+        args.add("--relative");
+        for (String subDir : subDirs)
+          args.add(srcDir.getAbsolutePath() + "/./" + subDir);
+      }
+      else
+        args.add(srcDir.getAbsolutePath() + "/");
+
+      // Finally add the destination path
+      args.add(dstDir.getAbsolutePath() + "/");
+
+      // And run the command
+      String[] argArray = args.toArray(new String[args.size()]);
+      ProcessRunner.runAndGrab(argArray, "", 0);
+    } 
+    catch (InterruptedException e) {
       throw new RuntimeException(e);
-    } catch (CommandFailedException e) {
+    } 
+    catch (CommandFailedException e) {
       throw new IOException(e.getMessage());
     }
-    
-    // Now that we've copied all the files, fix up the directory mod times.
-    assert fixDirs.size() == fixTimes.size(); // Should have added in parallel
-    for (int i = 0; i < fixDirs.size(); i++) {
-      File dstDir = fixDirs.get(i);
-      long time = fixTimes.get(i);
-      if (!dstDir.setLastModified(time))
-        throw new IOException("Error setting last modified date on dir '" + dstDir + "'");
-    }
-  }
-
-  /**
-   * Get a sorted list of the files (and subdirectories) within a directory.
-   */
-  private static String[] readSortedDir(File dir)
-  {
-    String[] list = dir.list();
-    Collections.sort(Arrays.asList(list));
-    String prev = null;
-    for (String s : list) {
-      assert prev == null || s.compareTo(prev) >= 0;
-      prev = s;
-    }
-    return list;
   }
 }
