0ffc28e88d8f1157209e63827d5aacd7d6db5511
Lisa Schiff <lschiff>
2011-04-04 10:57:53 -0700

    created Windows regressionTest.bat

---
+++ bin/regressTest.bat
@@ -0,0 +1,28 @@
+:: Batch file to run the XTF regression tests on Windows
+
+:: First, let's check to make sure XTF_HOME is set properly
+@echo off
+if not exist "%XTF_HOME%" goto NEED_HOME
+
+:: Make a classpath containing all the jars in XTF_HOME\WEB-INF\lib
+setlocal enabledelayedexpansion
+set XTFCP=%CLASSPATH%;%XTF_HOME%\WEB-INF\classes
+for %%i in ("%XTF_HOME%"\WEB-INF\lib\*.jar) do set XTFCP=!XTFCP!;%%i
+
+:: And fire off the command.
+cd %XTF_HOME%\regress\CrossQuery
+java -classpath "%XTFCP%" -Xms50m -Xmx500m -enableassertions org.cdlib.xtf.test.RegressTest %*
+
+set XTFCP=
+exit /B 0
+                                          
+:: Print error message and exit
+:NEED_HOME
+echo ERROR: XTF_HOME environment variable not set properly.
+echo You can set it in the "System" Control Panel; look on the Advanced tab,
+echo under "Environment Variables".
+echo Note that the value should not include quotes.
+exit /B 1
+
+
+                                
