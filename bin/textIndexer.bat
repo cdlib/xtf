6baec423e2619d2f97aa5a9982f522265afd41fb
Martin Haye <martin.haye@ucop.edu>
2016-03-30 11:17:19 -0700

    Little fixes to the Windows batch file, per BillTheGoat.

--- bin/textIndexer.bat
+++ bin/textIndexer.bat
@@ -2,7 +2,7 @@
 
 :: set the home directory
 @echo off
-set XTF_HOME=../%~dp0
+set XTF_HOME=%~dp0..
 
 :: Make a classpath containing all the jars in XTF_HOME\WEB-INF\lib
 setlocal enabledelayedexpansion
@@ -13,4 +13,3 @@ for %%i in ("%XTF_HOME%"\WEB-INF\lib\*.jar) do set XTFCP=!XTFCP!;%%i
 java -cp "%XTFCP%" -Xms150m -Xmx1500m -Dxtf.home="%XTF_HOME%" -DentityExpansionLimit=256000 -enableassertions org.cdlib.xtf.textIndexer.TextIndexer %*
 set XTFCP=
 set XTF_HOME=
-exit
